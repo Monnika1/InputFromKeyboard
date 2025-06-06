103cc99f3987c1a6c7ee5b2db020268fcf07e9c9
Monnika1 <mgoryalova@gmail.com>
2025-06-06 16:05:21 +0300

    Commiting new solutions :)

---
+++ StudentGrades/StudentGrades.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<(string Name, string Grade)> students = new List<(string, string)>
+            {
+                ("Alice", "A"),
+                ("Bob", "B"),
+                ("Kiril", "A"),
+                ("Max", "C")
+            };
+            var groupedStudents = students
+                 .GroupBy(s => s.Grade).ToDictionary(g => g.Key, g => g.Select(s => s.Name).ToList());
+
+            foreach (var pair in groupedStudents)
+            {
+                Console.WriteLine($"{pair.Key}-{string.Join(", ", pair.Value)}");
+
+            }
+
+
+        }
+    }
+
+}
