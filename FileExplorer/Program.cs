41da6eb0f9254ad293c351864996c03a3b573bd8
Monnika1 <mgoryalova@gmail.com>
2025-06-11 02:09:54 +0300

    Adding file explorer

---
+++ FileExplorer/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace FileExplorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\Moni\OneDrive\Desktop\Shop\InputFromKeyboard";
+            ReadFilesAndDirectories(rootPath);
+            Console.ReadLine();
+
+        }
+        static void ReadFilesAndDirectories(string path)
+        {
+            try
+            {
+                Console.WriteLine("Directory: " + path);
+                var files = Directory.GetFiles(path);
+                Console.WriteLine("Files:");
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                var directories = Directory.GetDirectories(path);
+
+                foreach (var dir in directories)
+                {
+                    ReadFilesAndDirectories(dir);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
